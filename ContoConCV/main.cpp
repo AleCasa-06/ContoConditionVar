8cdd89fa58040ff7240759634bc9b630a7ac3de8
CasarottoAlessandro <115339459+AleCasa-06@users.noreply.github.com>
2024-03-13 19:34:35 +0100

    Non capisco perché non funziona.

---
+++ ContoConCV/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include "random"
+#include "thread"
+#include "mutex"
+#include "condition_variable"
+#include "chrono"
+
+
+
+using namespace std;
+
+
+condition_variable cv;
+mutex ml;
+int conto = 0;
+
+
+
+void AggiungiSoldi(){
+
+    this_thread::sleep_for(chrono::seconds(2));
+    lock_guard<mutex> lock(ml);
+
+    conto += 1000;
+
+    cout << "ho aggiunto un milli al conto" << endl;
+    cout << "saldo attuale: " << conto << endl;
+
+    cv.notify_one();
+
+}
+
+void Preleva(int conto, int soldi){
+    unique_lock<mutex> lock(ml);
+    cv.wait(lock,  [conto] {
+        if(conto <= 0){
+            cout << "POVERIIII" << endl;
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+    });
+
+    conto -= soldi;
+
+
+}
+
+
+
+int main() {
+
+
+    int n = random()%1000 + 1;
+    cout << "prelevo: " << n << " euro" << endl;
+    thread th1(AggiungiSoldi);
+    thread th2(Preleva, ref(conto), n);
+
+    th1.join();
+    th2.join();
+    return 0;
+}
+
